05086e951133ef5fd860cc64380452d124cc7136
paulopez78 <pau.lopez.78@gmail.com>
2018-12-10 22:35:11 +0100

    dotnet app version

---
+++ src/votingapp-dotnet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace VotingApp.Api
+{
+   public class Program
+    {
+        public static void Main(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+    }    
+}
---
+++ src/votingapp-dotnet/Startup.cs
@@ -0,0 +1,26 @@
+﻿using EasyWebSockets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using VotingApp.Domain;
+
+namespace VotingApp.Api
+{
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddSingleton(new Voting());
+      services.AddMvc();
+      services.AddEasyWebSockets();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+      app.UseExceptionHandler();
+      app.UseMvc();
+      app.UseEasyWebSockets();
+    }
+  }
+}
